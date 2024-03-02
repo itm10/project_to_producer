ca9dc24a07557094a2dca19c942ebda87d2b29bb
itm10 <ibrohim.islom10@hmail.com>
2024-03-02 17:48:28 +0500

    DONE

--- Mebel/settings.py
+++ Mebel/settings.py
@@ -83,11 +83,15 @@ WSGI_APPLICATION = 'Mebel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 SWAGGER_SETTINGS = {

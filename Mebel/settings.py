a45b2a8a36f00cf611b5fb31aa5ced057f09dc9d
itm10 <ibrohim.islom10@hmail.com>
2024-03-02 18:29:33 +0500

    DONEs

--- Mebel/settings.py
+++ Mebel/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mebel-0r0p.onrender.com']
+ALLOWED_HOSTS = ['mebel-0r0p.onrender.com', '172.22.70.95']
 
 
 # Application definition

5d8f7a22fd0b2c85b629d7589da8b03ea87f84da
itm10 <ibrohim.islom10@hmail.com>
2024-03-03 14:15:21 +0500

    Ready

--- Mebel/settings.py
+++ Mebel/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'main.middlewares.IPRestrictedMiddleware',
+    # 'main.middlewares.IPRestrictedMiddleware',
 ]
 
 ROOT_URLCONF = 'Mebel.urls'

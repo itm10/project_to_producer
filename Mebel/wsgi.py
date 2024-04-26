a67c7d1b8be049c4824872624ec62dea1cb28b27
itm10 <ibrohim.islom10@hmail.com>
2024-04-26 18:55:57 +0500

    Done

--- Mebel/wsgi.py
+++ Mebel/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rd project.
+WSGI config for Mebel project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rd.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mebel.settings')
 
 application = get_wsgi_application()

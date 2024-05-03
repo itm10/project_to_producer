5fb5f51d0210e1682cc6927179ad115b2d1793e2
itm10 <ibrohim.islom10@hmail.com>
2024-05-03 15:10:30 +0500

    Done changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@ then
     echo "Waiting for postgres..."
     wait-for-it $SQL_HOST:$SQL_PORT -t 30
     echo "PostgreSQL started"
+    python manage.py collectstatic --noinput
+    gunicorn --bind 0.0.0.0:8000 mebel.wsgi
 fi
+
 
 exec "$@"

1d056f77386589d0991e567b07e4fcedb41fc3db
itm10 <ibrohim.islom10@hmail.com>
2024-05-08 10:49:19 +0500

    Done

--- mebel/urls.py
+++ mebel/urls.py
@@ -23,16 +23,17 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf.urls.static import static
 from django.conf import settings
-
+from .swagger import BothHttpAndHttpsSchemaGenerator
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+        generator_class=BothHttpAndHttpsSchemaGenerator,
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
@@ -41,7 +42,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/defender/', include('defender.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('main.urls')),
+    path('api/  ', include('main.urls')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),

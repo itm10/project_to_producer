042dc1a08c5ebd172e5f796229c2adc898e1493a
itm10 <ibrohim.islom10@hmail.com>
2024-05-08 11:26:49 +0500

    Done

--- mebel/urls.py
+++ mebel/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
-        generator_class=BothHttpAndHttpsSchemaGenerator,
+        # generator_class=BothHttpAndHttpsSchemaGenerator,
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),

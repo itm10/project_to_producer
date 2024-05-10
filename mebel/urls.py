00719b94ed5abc4b7e2b0241df26b43eb8f68395
itm10 <ibrohim.islom10@hmail.com>
2024-05-10 12:34:05 +0500

    Done

--- mebel/urls.py
+++ mebel/urls.py
@@ -26,7 +26,7 @@ from django.conf import settings
 from .swagger import BothHttpAndHttpsSchemaGenerator
 
 schema_view = get_schema_view(
-   openapi.Info(
+    openapi.Info(
         title="Snippets API",
         default_version='v1',
         description="Test description",
@@ -34,19 +34,16 @@ schema_view = get_schema_view(
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
         # generator_class=BothHttpAndHttpsSchemaGenerator,
-   ),
+    ),
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
-
 
 urlpatterns = [
     path('admin/defender/', include('defender.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('main.urls')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0, schema_view='https'), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
